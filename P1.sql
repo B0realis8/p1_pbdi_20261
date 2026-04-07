6a80963b96e63eac371c60d9ba0d60e7108aa828
Renan Antonio <renan.amsouza8@gmail.com>
2026-04-07 10:09:00 -0300

    feat(p1): encontra passageiros de cherbourg com tarifa alta

--- P1.sql
+++ P1.sql
@@ -8,7 +8,6 @@
 -- deve desenvolver o script de criação da tabela. 
 -- Mensagem de commit: feat(p1): cria base e importa dados
 
--- Inserir apenas colunas necessárias
 
 DROP TABLE IF EXISTS titanic;
 CREATE TABLE titanic(
@@ -29,6 +28,14 @@ Embarked VARCHAR
 
 );
 
+ALTER TABLE titanic
+DROP COLUMN Name;
+DROP COLUMN Age;
+DROP COLUMN SibSp;
+DROP COLUMN Parch;
+DROP COLUMN Ticket;
+DROP COLUMN Cabin;
+
 
 -- Enunciado 2 - Sobrevivência em função da classe social 
 -- Escreva um cursor não vinculado que mostra o número de passageiros sobreviventes que 
@@ -38,13 +45,13 @@ Embarked VARCHAR
 DO $$
 DECLARE
     cur REFCURSOR;
-    var_nome_passageiro INT;
+    var_passageiro INT;
     passageiros INT := 0;
 BEGIN
-    OPEN cur FOR SELECT Name FROM titanic WHERE Survived = true AND Pclass = 1;
+    OPEN cur FOR SELECT PassengerId FROM titanic WHERE Survived = true AND Pclass = 1;
         WHILE FOUND LOOP
             passageiros := n_passageiros + 1;
-            FETCH cur INTO var_nome_passageiro;
+            FETCH cur INTO var_passageiro;
             EXIT WHEN NOT FOUND;
         END LOOP;
         RAISE NOTICE 'Passageiros sobreviventes que viajavam na primeira classe: %',passageiros;
@@ -81,3 +88,26 @@ BEGIN
     CLOSE cur;
 END;
 $$
+
+-- Enunciado 4 -  Tarifa versus embarque 
+-- Dentre os passageiros que pagaram tarifa (Fare) maior que 50, quantos embarcaram em 
+-- Cherbourg (Embarked = 'C')? Escreva um cursor vinculado que exiba esse valor. 
+-- Mensagem de commit: feat(p1): encontra passageiros de Cherbourg com tarifa alta
+
+DO $$
+DECLARE
+    cur CURSOR FOR SELECT PassengerId FROM titanic WHERE Fare > 50 AND Embarked = 'C';
+    passageiros INT := 0;
+    var_id_passageiro INT;
+BEGIN
+    OPEN cur;
+        FETCH cur INTO var_id_passageiro;
+        WHILE FOUND LOOP
+            passageiros := passageiros + 1;
+            FETCH cur INTO var_id_passageiro;
+        END LOOP;
+    CLOSE cur;
+    RAISE NOTICE 'Passageiros que pagaram tarifa > 50 e embarcaram e Cherbourg: %',passageiros;
+END;
+$$
+
